f322bc1dd2f7727ec04dfedbd17184971b00d931
Vaishnavi-VKN <vaishnavi.vn06@gmail.com>
2026-02-25 16:32:29 +0530

    Added C++ algorithm programs

---
+++ Insertion_Sort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];      // Element to be inserted
+        int j = i - 1;
+
+        // Move elements that are greater than key
+        // to one position ahead
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;  // Place key at correct position
+    }
+}
+
+int main() {
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    insertionSort(arr, n);
+
+    cout << "Sorted array:\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
